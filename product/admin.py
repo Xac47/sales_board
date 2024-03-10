331cebf0b1d6f266d278e08c5d3a2563ff986219
Xac47 <hasbulat69@gmail.com>
2024-03-11 00:36:23 +0300

    add model Brand in admin

--- product/admin.py
+++ product/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 
 from mptt.admin import DraggableMPTTAdmin
-from .models import Category
+from .models import Category, Brand
 
 
 @admin.register(Category)
 class CategoryAdmin(DraggableMPTTAdmin):
     list_display = ('tree_actions', 'indented_title', 'id', 'name', 'slug')
-    list_display_links = ('name', 'slug')
+    list_display_links = ('name', 'slug', 'id', 'indented_title')
+
+    fieldsets = (
+        ('Основная информация', {'fields': ('name', 'slug', 'parent')}),
+    )
+
+
+@admin.register(Brand)
+class BrandAdmin(DraggableMPTTAdmin):
+    list_display = ('tree_actions', 'indented_title', 'id', 'name', 'slug')
+    list_display_links = ('name', 'slug', 'id', 'indented_title')
 
     fieldsets = (
         ('Основная информация', {'fields': ('name', 'slug', 'parent')}),
