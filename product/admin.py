4cfd4f21cb07dfa72e7fa567e607b5a236ddac3e
Xac47 <hasbulat69@gmail.com>
2024-03-11 00:12:22 +0300

    add model Category in admin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from mptt.admin import DraggableMPTTAdmin
+from .models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(DraggableMPTTAdmin):
+    list_display = ('tree_actions', 'indented_title', 'id', 'name', 'slug')
+    list_display_links = ('name', 'slug')
+
+    fieldsets = (
+        ('Основная информация', {'fields': ('name', 'slug', 'parent')}),
+    )
