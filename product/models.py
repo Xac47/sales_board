e0bb691d2469e8205f06d138549056f2373821a6
Xac47 <hasbulat69@gmail.com>
2024-03-11 00:39:54 +0300

    create model Brand

--- product/models.py
+++ product/models.py
@@ -34,6 +34,33 @@ class Category(MPTTModel):
         self.slug = f'{slugify(translit_to_eng(self.name))}'
         super().save(*args, **kwargs)
 
-# class Product(models.Model):
-#     desc = models.TextField('Описание')
-#     price = models.DecimalField('Цена', decimal_places=2, max_digits=10)
+
+class Brand(MPTTModel):
+    name = models.CharField('Название', max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='slug', blank=True)
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        db_index=True,
+        related_name='children',
+        verbose_name='Родитель'
+    )
+
+    class MPTTMeta:
+        """ Сортировка по вложенности """
+        order_insertion_by = ('name',)
+
+    class Meta:
+        verbose_name = 'Бренд'
+        verbose_name_plural = 'Бренды'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = f'{slugify(translit_to_eng(self.name))}'
+        super().save(*args, **kwargs)
+
+
