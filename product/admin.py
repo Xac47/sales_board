cd02e80bbdaf1273a09d78b0d59bf60bcfd7d5e5
Xac47 <hasbulat69@gmail.com>
2024-03-17 01:31:42 +0300

    add BrandModelInline, BrandAdmin, BrandModelAdmin, ProductAdmin, ProductAdminForm

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-
+from django import forms
 from mptt.admin import DraggableMPTTAdmin
-from .models import Category, Brand
+from mptt.forms import TreeNodeChoiceField
+from .models import Category, Brand, Product, BrandModel
 
 
 @admin.register(Category)
@@ -14,11 +15,41 @@ class CategoryAdmin(DraggableMPTTAdmin):
     )
 
 
-@admin.register(Brand)
-class BrandAdmin(DraggableMPTTAdmin):
-    list_display = ('tree_actions', 'indented_title', 'id', 'name', 'slug')
-    list_display_links = ('name', 'slug', 'id', 'indented_title')
+class BrandModelInline(admin.TabularInline):
+    model = BrandModel
 
-    fieldsets = (
-        ('Основная информация', {'fields': ('name', 'slug', 'parent')}),
-    )
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    list_display_links = ('id', 'name', 'slug')
+    inlines = (BrandModelInline,)
+
+
+@admin.register(BrandModel)
+class BrandModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'brand', 'model_name', 'slug')
+    list_display_links = ('id', 'brand', 'model_name', 'slug')
+
+
+class ProductAdminForm(forms.ModelForm):
+    category = TreeNodeChoiceField(queryset=Category.objects.all(), empty_label='Выберите категорию')
+    # brand = forms.ModelChoiceField(queryset=Brand.objects.all(), empty_label='Выберите бренд')
+
+    brand = forms.ModelChoiceField(queryset=Brand.objects.all(), empty_label='Выберите бренд',
+                                   widget=forms.Select(attrs={'onchange': 'load_brand_models();', 'id': 'id_brand'}))
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+        widgets = {
+            'brand_model': forms.Select(attrs={'id': 'id_brand_model'}),
+        }
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'desc', 'price', 'category', 'brand')
+    list_display_links = ('id', 'desc')
+
+    form = ProductAdminForm
